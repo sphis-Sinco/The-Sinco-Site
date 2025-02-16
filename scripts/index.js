c91855390026ae5b1b38fb5d86467d7445fdb9e2
IdealistCat <sinconsistencia@gmail.com>
2025-02-16 00:31:20 -0600

    hello world text

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,12 @@
 import { appendFooter } from "../modules/pages/footer.js";
 
+var holder = document.createElement('p');
+holder.className = 'style';
+
+var greeting_text = document.createElement('p');
+greeting_text.innerHTML = "Hello world!"
+holder.appendChild(greeting_text);
+
+document.body.appendChild(holder);
+
 appendFooter();
